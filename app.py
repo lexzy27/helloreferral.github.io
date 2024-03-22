c84a079714affeef8bece8270c34735025092cdc
lexzy27 <134909658+lexzy27@users.noreply.github.com>
2024-03-22 22:20:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
-@app.route('/')
-def hello_referral():
-    return "<h1>Hello Referral!</h1>"
+
+
+@app.route("/")
+@app.route("/hello")
+def hello():
+    return render_template('hello.html')
+
+@app.route('/display')
+def display():
+ return render_template('display.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
     
