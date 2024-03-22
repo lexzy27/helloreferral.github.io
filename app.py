48ec13443c8a7bc7410cedbe90b7e196cad6d09b
lexzy27 <134909658+lexzy27@users.noreply.github.com>
2024-03-22 21:25:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_referral():
-    return "hello_referral!"
+    return "Hello Referral!"
 if __name__ == '__main__':
     app.run(debug=True)
     
